6756aa84c86028d75eeae2b2211414abf6dc9e1d
michalaforest3 <200252967@student.georgianc.on.ca>
2023-12-20 14:43:42 -0500

    Changed the car trim table data to use objects for each car trim (in javascript)

--- js/main.js
+++ js/main.js
@@ -44,8 +44,6 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
                 }
             }
         }
-
-
     }
 
     /* --------------------------------------- */
@@ -239,15 +237,14 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
         let yw = new CarColor("yw-", "yellow", "Mythos Black Metallic");
         let rd = new CarColor("rd-", "red", "Mythos Black Metallic");
 
-        let allSquaresOrdered = [bkSq, chSq, slSq, whSq, nvSq, blSq, ywSq, rdSq];
         let allColorsOrdered = [bk, ch, sl, wh, nv, bl, yw, rd];
 
         // Add an event listener to each color square, and invoke the bodyColorChange function when a square is clicked
-        for (let i = 0; i < allSquaresOrdered.length; i++) {
-            allSquaresOrdered[i].addEventListener("click", function() {
+        for (let i = 0; i < allColorSq.length; i++) {
+            allColorSq[i].addEventListener("click", function() {
 
                 let currentColor = allColorsOrdered[i];
-                bodyColorChange(currentColor["colorCode"], currentColor["colorName"], allSquaresOrdered[i], currentColor["fullColorName"]);
+                bodyColorChange(currentColor["colorCode"], currentColor["colorName"], allColorSq[i], currentColor["fullColorName"]);
             });
         }
 
@@ -401,6 +398,7 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
         }
     }
     window.addEventListener('resize', removeMenuTransition);
+
     /* --------------------------------------- */
     /*    CAR CONFIGURATION DROP DOWN MENU     */
     /* --------------------------------------- */
@@ -415,7 +413,7 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
         var configMenu = document.getElementById("config-menu");
         var configMenuValue = document.getElementById("config-menu").value;
 
-        // get the table value elements
+        // get the table value elements (desktop and mobile tables)
         var configPrice = document.querySelectorAll(".config-price");
         var configEngine = document.querySelectorAll(".config-engine");
         var configTransmission = document.querySelectorAll(".config-transmission");
@@ -424,6 +422,49 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
         var configHP = document.querySelectorAll(".config-hp");
         var configTorque = document.querySelectorAll(".config-torque");
 
+        // Create a CarTrim class
+        class CarTrim {
+            constructor(name, price, engine, transmission, driveTrain, fuelCity, fuelHwy, hp, torque) {
+                this.name = name;
+                this.price = price;
+                this.engine = engine;
+                this.transmission = transmission;
+                this.driveTrain = driveTrain;
+                this.fuelCity = fuelCity;
+                this.fuelHwy = fuelHwy;
+                this.hp = hp;
+                this.torque = torque;
+            }
+        }
+
+        // Declare the CarTrim objects
+        let coupeV10RWD = new CarTrim("Coupe V10 Performance RWD", "$158,600", "5.2L V10 Gas", "7-Speed Automatic", "Rear-Wheel Drive", "14", "23", "562 HP @ 4475 RPM", "406 lb-ft @ 6300 RPM");
+        let coupeV10quattro = new CarTrim("Coupe V10 Performance Quattro", "$209,700", "5.2L V10 Gas", "7-Speed Automatic", "All-Wheel Drive", "13", "18", "602 HP @ 8100 RPM", "413 lb-ft @ 6700 RPM");
+        let coupeGTRWD = new CarTrim("Coupe GT RWD", "$249,900", "5.2L V10 Gas", "7-Speed Automatic", "Rear-Wheel Drive", "14", "21", "602 HP @ 8100 RPM", "413 lb-ft @ 6700 RPM");
+        let spyderV10RWD = new CarTrim("Spyder V10 Performance RWD", "$171,000", "5.2L V10 Gas", "7-Speed Automatic", "Rear-Wheel Drive", "14", "23", "562 HP @ 4475 RPM", "406 lb-ft @ 6300 RPM");
+        let spyderV10Quattro = new CarTrim("Spyder V10 Performance Quattro", "$222,100", "5.2L V10 Gas", "7-Speed Automatic", "All-Wheel Drive", "13", "18", "602 HP @ 8100 RPM", "413 lb-ft @ 6700 RPM");
+
+        // Function to update the values in the config table
+        function updateTableData(carTrim) {
+            configPrice[0].textContent = carTrim["price"];
+            configPrice[1].textContent = carTrim["price"];
+            configEngine[0].textContent = carTrim["engine"];
+            configEngine[1].textContent = carTrim["engine"];
+            configTransmission[0].textContent = carTrim["transmission"];
+            configTransmission[1].textContent = carTrim["transmission"];
+            configDrivetrain[0].textContent = carTrim["driveTrain"];
+            configDrivetrain[1].textContent = carTrim["driveTrain"];
+            configFuel[0].innerHTML = "City: " + carTrim["fuelCity"] + " MPG\n" +
+                "                    <span>Hwy: " + carTrim["fuelHwy"] + " MPG</span>";
+            configFuel[1].innerHTML = "City: " + carTrim["fuelCity"] + " MPG\n" +
+                "                    <span>Hwy: " + carTrim["fuelHwy"] + " MPG</span>";
+            configHP[0].textContent = carTrim["hp"];
+            configHP[1].textContent = carTrim["hp"];
+            configTorque[0].textContent = carTrim["torque"];
+            configTorque[1].textContent = carTrim["torque"];
+            configSelected.textContent = carTrim["name"];
+        }
+
         // function to update the configuration table based on which value is selected from dropdown menu
         function updateConfigTable() {
             configMenuValue = document.getElementById("config-menu").value;
@@ -431,103 +472,19 @@ document.addEventListener('DOMContentLoaded',(loaded) => {
             // switch statement to display table values based on option selected
             switch(configMenuValue) {
                 case "1":
-                    configPrice[0].textContent = "$158,600";
-                    configPrice[1].textContent = "$158,600";
-                    configEngine[0].textContent = "5.2L V10 Gas";
-                    configEngine[1].textContent = "5.2L V10 Gas";
-                    configTransmission[0].textContent = "7-Speed Automatic";
-                    configTransmission[1].textContent = "7-Speed Automatic";
-                    configDrivetrain[0].textContent = "Rear-Wheel Drive";
-                    configDrivetrain[1].textContent = "Rear-Wheel Drive";
-                    configFuel[0].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 23 MPG</span>";
-                    configFuel[1].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 23 MPG</span>";
-                    configHP[0].textContent = "562 hp @ 4475 rpm";
-                    configHP[1].textContent = "562 hp @ 4475 rpm";
-                    configTorque[0].textContent = "406 lb-ft @ 6300 rpm";
-                    configTorque[1].textContent = "406 lb-ft @ 6300 rpm";
-                    configSelected.textContent = "Coupe V10 performance RWD";
+                    updateTableData(coupeV10RWD);
                     break;
-
                 case "2":
-                    configPrice[0].textContent = "$209,700";
-                    configPrice[1].textContent = "$209,700";
-                    configEngine[0].textContent = "5.2L V10 Gas";
-                    configEngine[1].textContent = "5.2L V10 Gas";
-                    configTransmission[0].textContent = "7-Speed Automatic";
-                    configTransmission[1].textContent = "7-Speed Automatic";
-                    configDrivetrain[0].textContent = "All-Wheel Drive";
-                    configDrivetrain[1].textContent = "All-Wheel Drive";
-                    configFuel[0].innerHTML = "City: 13 MPG\n" +
-                        "                    <span>Hwy: 18 MPG</span>";
-                    configFuel[1].innerHTML = "City: 13 MPG\n" +
-                        "                    <span>Hwy: 18 MPG</span>";
-                    configHP[0].textContent = "602 hp @ 8100 rpm";
-                    configHP[1].textContent = "602 hp @ 8100 rpm";
-                    configTorque[0].textContent = "413 lb-ft @ 6700 rpm";
-                    configTorque[1].textContent = "413 lb-ft @ 6700 rpm";
-                    configSelected.textContent = "Coupe V10 performance quattro";
+                    updateTableData(coupeV10quattro);
                     break;
-
                 case "3":
-                    configPrice[0].textContent = "$249,900";
-                    configPrice[1].textContent = "$249,900";
-                    configEngine[0].textContent = "5.2L V10 Gas";
-                    configEngine[1].textContent = "5.2L V10 Gas";
-                    configTransmission[0].textContent = "7-Speed Automatic";
-                    configTransmission[1].textContent = "7-Speed Automatic";
-                    configDrivetrain[0].textContent = "Rear-Wheel Drive";
-                    configDrivetrain[1].textContent = "Rear-Wheel Drive";
-                    configFuel[0].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 21 MPG</span>";
-                    configFuel[1].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 21 MPG</span>";
-                    configHP[0].textContent = "602 hp @ 8100 rpm";
-                    configHP[1].textContent = "602 hp @ 8100 rpm";
-                    configTorque[0].textContent = "413 lb-ft @ 6700 rpm";
-                    configTorque[1].textContent = "413 lb-ft @ 6700 rpm";
-                    configSelected.textContent = "Coupe GT RWD";
+                    updateTableData(coupeGTRWD);
                     break;
-
                 case "4":
-                    configPrice[0].textContent = "$171,000";
-                    configPrice[1].textContent = "$171,000";
-                    configEngine[0].textContent = "5.2L V10 Gas";
-                    configEngine[1].textContent = "5.2L V10 Gas";
-                    configTransmission[0].textContent = "7-Speed Automatic";
-                    configTransmission[1].textContent = "7-Speed Automatic";
-                    configDrivetrain[0].textContent = "Rear-Wheel Drive";
-                    configDrivetrain[1].textContent = "Rear-Wheel Drive";
-                    configFuel[0].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 23 MPG</span>";
-                    configFuel[1].innerHTML = "City: 14 MPG\n" +
-                        "                    <span>Hwy: 23 MPG</span>";
-                    configHP[0].textContent = "562 hp @ 4475 rpm";
-                    configHP[1].textContent = "562 hp @ 4475 rpm";
-                    configTorque[0].textContent = "406 lb-ft @ 6300 rpm";
-                    configTorque[1].textContent = "406 lb-ft @ 6300 rpm";
-                    configSelected.textContent = "Spyder V10 performance RWD";
+                    updateTableData(spyderV10RWD);
                     break;
-
                 case "5":
-                    configPrice[0].textContent = "$222,100";
-                    configPrice[1].textContent = "$222,100";
-                    configEngine[0].textContent = "5.2L V10 Gas";
-                    configEngine[1].textContent = "5.2L V10 Gas";
-                    configTransmission[0].textContent = "7-Speed Automatic";
-                    configTransmission[1].textContent = "7-Speed Automatic";
-                    configDrivetrain[0].textContent = "All-Wheel Drive";
-                    configDrivetrain[1].textContent = "All-Wheel Drive";
-                    configFuel[0].innerHTML = "City: 13 MPG\n" +
-                        "                    <span>Hwy: 18 MPG</span>";
-                    configFuel[1].innerHTML = "City: 13 MPG\n" +
-                        "                    <span>Hwy: 18 MPG</span>";
-                    configHP[0].textContent = "602 hp @ 8100 rpm";
-                    configHP[1].textContent = "602 hp @ 8100 rpm";
-                    configTorque[0].textContent = "413 lb-ft @ 6700 rpm";
-                    configTorque[1].textContent = "413 lb-ft @ 6700 rpm";
-                    configSelected.textContent = "Spyder V10 performance quattro";
+                    updateTableData(spyderV10Quattro);
                     break;
             }
         }
